df35fee2b166368f76e6121b5875c79820e85c4b
Claude Mokbel <claudemokbel@gmail.com>
2023-01-10 11:19:20 -0800

    remove redundant container class

--- src/Views/About/Pages/Clergy.js
+++ src/Views/About/Pages/Clergy.js
@@ -7,13 +7,13 @@ export const Clergy = () => {
                 {clergy.members.map((member, index) => {
                     return (
                         <li key={index} className="row clergy-member">
-                            <div className="left-column container col-md-3 col-sm-12">
+                            <div className="left-column col-md-3 col-sm-12">
                                 <img src={member.photo} alt={member.name}></img>
                                 <h3>{member.title}</h3>
                                 <h3>{member.name}</h3>
                                 <h4>{member.subtitle ? member.subtitle : null }</h4>
                             </div>
-                            <div className="right-column container col-md-6 col-sm-12">
+                            <div className="right-column col-md-6 col-sm-12">
                                 <p>{member.bio}</p>
                             </div>
                         </li>
